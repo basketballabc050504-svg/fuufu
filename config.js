a312376eb95f83806c386a042aaeca8c10aab5dd
basketballabc050504-svg <basketball.abc050504@gmail.com>
2026-06-27 18:01:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.SHOPPING_LIST_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://qkpexusyfxjqewvmpbvd.supabase.co",
+  supabaseAnonKey: "sb_publishable__CE7VTwEKbiBxCBdcuz5FA_F5UAxdBx",
 };
